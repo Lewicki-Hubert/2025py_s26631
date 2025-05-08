48b9b510a5dc55a97fc8323be8332f4cc8d34cd4
Lewicki-Hubert <166704327+Lewicki-Hubert@users.noreply.github.com>
2025-05-08 17:11:34 +0200

    main bez modyfikacji

---
+++ s26631_2025.py
@@ -0,0 +1,43 @@
+import random
+
+# Cel programu: Generowanie syntetycznych sekwencji DNA w formacie FASTA dla celów testowych w bioinformatyce
+# Kontekst: Narzędzie do tworzenia danych treningowych dla algorytmów analizy sekwencji DNA
+
+seq_length = int(input("Podaj długość sekwencji: "))
+seq_id = input("Podaj ID sekwencji: ").strip()
+description = input("Podaj opis sekwencji: ")
+name = input("Podaj imię: ")
+
+nucleotides = ['A', 'C', 'G', 'T']
+dna_seq = ''.join(random.choice(nucleotides) for _ in range(seq_length))
+
+insert_index = random.randint(0, len(dna_seq))
+final_seq = dna_seq[:insert_index] + name + dna_seq[insert_index:]
+
+filename = f"{seq_id}.fasta"
+
+with open(filename, 'w') as file:
+    file.write(f">{seq_id} {description}\n")
+    file.write(final_seq + "\n")
+
+print(f"Sekwencja została zapisana do pliku {filename}")
+
+count_A = dna_seq.count('A')
+count_C = dna_seq.count('C')
+count_G = dna_seq.count('G')
+count_T = dna_seq.count('T')
+total = len(dna_seq)
+
+perc_A = (count_A / total) * 100
+perc_C = (count_C / total) * 100
+perc_G = (count_G / total) * 100
+perc_T = (count_T / total) * 100
+
+cg_at_ratio = ((count_C + count_G) / (count_A + count_T)) * 100 if (count_A + count_T) != 0 else 0
+
+print("Statystyki sekwencji:")
+print(f"A: {perc_A:.1f}%")
+print(f"C: {perc_C:.1f}%")
+print(f"G: {perc_G:.1f}%")
+print(f"T: {perc_T:.1f}%")
+print(f"%CG: {cg_at_ratio:.1f}")
